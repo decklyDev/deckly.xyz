098be9024ea934d37210d0fd6626a35130d41768
Janakiraman NPR <nprjanakiraman1987@gmail.com>
2018-04-01 22:35:43 +0530

    user registry done without styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: '', path_names: {sign_in: 'login',sign_out: 'logout',sign_up: 'register'}
+  devise_for :users, path: '', path_names: {sign_in: 'login',sign_out: 'logout',sign_up: 'register'},controllers: { confirmations: 'confirmations' }
   get 'pages/contact'
   get 'pages/blog'
   root to: 'pages#home'
